17994f59dd7aae05824046af2497e77624fa525f
Kiet Nguyen <funsmurf@Funs-Mac.local>
2017-04-13 23:45:23 +0700

    Add checkPoint, label sign, label temp number

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,7 +11,11 @@ import UIKit
 class ViewController: UIViewController {
     
     
-    @IBOutlet weak var txtNumber: UILabel!
+    @IBOutlet weak var txtTempNumber: UILabel!
+    @IBOutlet weak var txtSign: UILabel!
+    @IBOutlet weak var txtResult: UILabel!
+    
+    var result: Double = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,11 +25,71 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
     // 0 -> 9 button action
-    
     @IBAction func btnNumber(_ sender: UIButton) {
-        txtNumber.text = txtNumber.text! + String(sender.tag-1)
+        if (txtResult.text == "0"){
+            txtResult.text = String(sender.tag)
+        }
+        else{
+            txtResult.text = txtResult.text! + String(sender.tag)
+        }
     }
+    
+    // other button action
+    
+    @IBAction func btnMath(_ sender: UIButton) {
+    }
+    
+    
+    
+    // Button "." action
+    @IBAction func btnPoint(_ sender: Any) {
+        if (!checkPoint()) {
+            let alert = UIAlertController(title: "Error", message: "Wrong Format!!!", preferredStyle: UIAlertControllerStyle.alert);
+            //add an action
+            alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil));
+            //show alert
+            self.present(alert, animated: true, completion: nil);
+        }
+        else{
+            txtResult.text = txtResult.text! + "."
+        }
+        /*result = Double(txtResult.text!)!
+        NSLog(String(result))*/
+    }
+    
+    func checkPoint() -> Bool{
+        var countDot : Int = 0
+        for character in (txtResult.attributedText?.string.characters)!
+        {
+            if(character == ".")
+            {
+                countDot += 1
+            }
+        }
+        if (countDot == 0){
+            return true
+        }
+        else{
+            return false
+        }
+    }
+    
+   /* func checkCharacter(character: String) -> Bool {
+        var countDot : Int = 0
+        for character in (txtResult.attributedText?.string.characters)!
+        {
+            if(character == ".")
+            {
+                countDot += 1
+            }
+        }
+        if (countDot > 1) {
+            return false
+        }
+        else{
+            return true
+        }
+        return true
+    }*/
 }
-
